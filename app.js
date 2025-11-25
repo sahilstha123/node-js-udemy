6a5a340a9c93c2c803fef1788bd7e7eed113a388
sahilstha123 <sthasahil401@gmail.com>
2025-11-25 15:43:34 +0545

    handle 404 error page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,7 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<p>Page not Found<p>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 app.listen(3000, () => {
   console.log("Server has started on ");

b3be6c8665b92d5ef98ba5a90f0ba9b324c52573
sahilstha123 <sthasahil401@gmail.com>
2025-11-25 15:41:09 +0545

    handle paths using path module

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ app.use(express.urlencoded({ extended: true }));
 
 const adminRoutes = require("./routes/admin.js");
 
+const shopRoutes = require("./routes/shop.js");
+
 app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send("<p>Page not Found<p>");

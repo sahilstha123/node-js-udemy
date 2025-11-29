dd47147521c9649ebc6f98d2f53bea72cabf3ef3
Sahil shrestha <sthasahil401@gmail.com>
2025-11-29 21:03:07 +0545

    update all views and public file

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("Server has started on ");
 });
